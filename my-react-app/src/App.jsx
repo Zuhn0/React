56fc8398940d281eb70468dc6adb3afb509f5fdb
Zuhn0 <harshsingh0063@gmail.com>
2025-11-01 10:36:28 +0000

    Conditional_rendereing

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,12 @@
-//props = read-only properties that are shared between components
-//        A parent component can send data to a child component
-//        <Component Key=value  />
+//Conditional rendering = allows you to control what gets rendered 
+//                        in your application based on certain conditions
+//                        (show,hide, or change components)
 
-
-import Student from './Student.jsx'
+import UserGreeting from './UserGreeting.jsx'
 function App() {
    return(
       <>
-     <Student name="Spongebob" age={30} isStudent={true}/>
-     <Student name="Patrick" age={40} isStudent={false}/>
-     <Student name="Squidward" age={50} isStudent={true}/>
-     <Student />
+       <UserGreeting isLoggedIn={true} />
       </>
    );
 }
