826a8140d9cbbbb8caa00e8eec1af81b063930bd
Zuhn0 <harshsingh0063@gmail.com>
2025-11-03 13:19:46 +0000

    use_State_hook

---
+++ my-react-app/src/MyComponent.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from 'react';
+
+function MyComponent(){
+   const [name, setName] = useState("Guest"); 
+   const [age, setAge]= useState(0)
+   const [isEmployed,setIsEmployed]=useState(false);
+   const updateName= () => {
+       setName("Spongebob");
+   }
+      const IncrementAge= () => {
+       setAge(age+1);
+   }
+
+   const toggleEmployedStatus=()=>{
+         setIsEmployed(!isEmployed);
+   }
+   return (
+    <div>
+        <p>Name: {name} </p>
+        <button onClick={updateName}>Set Name</button>
+
+        <p>Age: {age} </p>
+        <button onClick={IncrementAge}>Increment Age</button>
+
+        <p>IsEmployed: {isEmployed? "YES" : "NO"}</p>
+        <button onClick={toggleEmployedStatus}>Toggle Status</button>
+    </div>
+   );
+}
+export default MyComponent
--- my-react-app/src/ProfilePicture.jsx
+++
@@ -1,6 +0,0 @@
-function ProfilePicture(){
-  const imageurl='./src/assets/image.jpg'
-  const handleClick=(e)=>e.target.style.display="none";
-  return(<img onClick={(e)=>handleClick(e)} src={imageurl}></img>)
-}
-export default ProfilePicture
