e8102330bf1b7c2b18f8b5e9f5e8285c5e1c5e51
Zuhn0 <harshsingh0063@gmail.com>
2025-12-15 10:42:37 +0000

    On_change_event_handler

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,33 +1,13 @@
-//React hook = Special function that allows functional components
-//             to use React features without writing class components
-//             (useState , useEffect, useContext,useReducer, useCallback, and more...)
+import MyComponent from "./MyComponent";
 
-//useState()=A React hook that allows the creation of a stateful variable
-//           AND a setter function to update its value in the virtual DOM.
-//           [name, setName]
-
-// import MyComponent from "./MyComponent";
-
-
-// function App(){
-
-//      return (
-//           <>
-//           <MyComponent></MyComponent>
-//           </>
-//      )
-// }
-
-// export default App
-
-import Counter from './Counter.jsx'
 
 function App(){
+
      return (
           <>
-          <Counter></Counter>
+          <MyComponent></MyComponent>
           </>
-          
      )
 }
+
 export default App
--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,30 +1,67 @@
-import React,{useState} from 'react';
-
+//onChange = event handler used primarily with form elements
+//          ex. <input>, <textarea>, <select>, <radio>
+//          Triggers a function every time the value of the input changes
+import React, {useState} from "react";
 function MyComponent(){
-   const [name, setName] = useState("Guest"); 
-   const [age, setAge]= useState(0)
-   const [isEmployed,setIsEmployed]=useState(false);
-   const updateName= () => {
-       setName("Spongebob");
-   }
-      const IncrementAge= () => {
-       setAge(age+1);
-   }
+    
+     const [name,setName]=useState("Guest");
+     const [quantity,setQuantity]=useState(1);
+     const [comment,setComment]=useState("");
+     const [payment,setPayment]=useState("");
+     const [shipping,setShipping]=useState("Delivery");
 
-   const toggleEmployedStatus=()=>{
-         setIsEmployed(!isEmployed);
-   }
-   return (
-    <div>
-        <p>Name: {name} </p>
-        <button onClick={updateName}>Set Name</button>
+     function handleNameChange(event){
+        setName(event.target.value);
+     }
 
-        <p>Age: {age} </p>
-        <button onClick={IncrementAge}>Increment Age</button>
+     function handleQuantityChange(event)
+     {
+      setQuantity(event.target.value);
+     }
 
-        <p>IsEmployed: {isEmployed? "YES" : "NO"}</p>
-        <button onClick={toggleEmployedStatus}>Toggle Status</button>
-    </div>
-   );
+     function handleCommentChange(event){
+      setComment(event.target.value);
+     }
+
+     function handlePaymentChange(event){
+      setPayment(event.target.value);
+     }
+     
+     function handleShippingChange(event){
+      setShipping(event.target.value); 
+     }
+
+       return (<div>
+          <input value={name} onChange={handleNameChange}></input>
+          <p>Name: {name}</p>
+
+          <input value={quantity} onChange={handleQuantityChange} type="number"/>
+          <p>Quantity: {quantity}</p>
+
+          <textarea value={comment} onChange={handleCommentChange} placeholder="Enter delivery Instructions"/>
+          <p>Comment: {comment}</p>
+
+          <select value={payment} onChange={handlePaymentChange}>
+          <option value="">select an option</option>
+          <option value="Visa">Visa</option>
+          <option value="Mastercard">Mastercard</option>
+          </select>
+          <p>Payment:{payment}</p>
+
+          <label>
+            <input type="radio" value="Pick Up"
+                   checked={shipping=== "Pick Up"}
+                   onChange={handleShippingChange}/>
+            Pick Up
+          </label><br/>
+          <label>
+                 <input type="radio" value="Delivery"
+                   checked={shipping=== "Delivery"}
+                   onChange={handleShippingChange}/>
+            Delivery
+          </label>
+          <p>Shipping: {shipping}</p>
+       </div>);
+   
 }
 export default MyComponent
