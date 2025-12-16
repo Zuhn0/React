f32ef18919e279c72df701ade7763d881eeee1d8
Zuhn0 <harshsingh0063@gmail.com>
2025-12-16 18:10:10 +0000

    update_state_of_Arrays

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -1,29 +1,34 @@
 import React, {useState} from 'react';
 
 function MyComponent(){
-    const [car, setCar] = useState({year: 2024,
-                                    make: "Nissan",
-                                    model: "GTR"});
     
-    function handleYearChange(event){
-        setCar(c=> ({...c, year: event.target.value}));
-        //...car is a spread  operator which || year: 2024,make: "Nissan",model: "GTR" , year:newlyset ||    it updates the year in that object
+    const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+     
+    function handleAddFood(){ 
+     
+    const newFood = document.getElementById("foodInput").value;
+    document.getElementById("foodInput").value="";
+    
+    setFoods (f => [...f, newFood]); //spread operator
     }
-        function handleMakeChange(event){
-        setCar(c => ({...c, make: event.target.value}));
-    }
-        function handleModelChange(event){
-        setCar(c => ({...c, model: event.target.value}));
+
+    function handleRemoveFood(index){
+      
+        setFoods(foods.filter((_,i) => i!=index)); //"_" means non required parameter
     }
 
     return (
-        <div>
-            <p>Your Favorite Car is : {car.year} {car.make} {car.model}</p>
-
-            <input type="number" value={car.year} onChange={handleYearChange} /> <br/>
-            <input type="text" value={car.make} onChange={handleMakeChange} /> <br/>
-            <input type="text" value={car.model} onChange={handleModelChange}/> <br/>
-        </div>
+     <div>
+            <h2>List Of Food</h2>
+            <ul>
+                {foods.map((food, index) => 
+                <li key={index} onClick={() => handleRemoveFood(index)}>
+                    {food}
+                    </li>)}
+            </ul>
+            <input type="text" id="foodInput" placeholder="Enter food name"/>
+            <button onClick={handleAddFood}>Add Food</button>
+     </div>
     );
 
 }
