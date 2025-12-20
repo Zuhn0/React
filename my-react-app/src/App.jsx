afb223d5b130bc40a1da221f73c804d29f09208b
Zuhn0 <harshsingh0063@gmail.com>
2025-12-20 13:27:07 +0000

    Use_Context_React_hook

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,26 @@
-import MyComponent from "./MyComponent.jsx";
-import "./index.css"
+//  useContext() =  React Hook that allows you to share values
+//                  between multiple levels of components
+//                  without passing props through each level
+
+//  PROVIDER COMPONENT
+// 1. import {createContext} from 'react';
+// 2. export const MyContext = createContext();
+// 3. <MyContext.Provider value={value}>
+// 4. <Child />
+//   </MyContext.Provider>
+
+//  CONSUMER COMPONENTS
+// 1.import React, { useContext } from 'react'
+//   import { MyContext } from './ComponentA';
+//
+import ComponentA from './ComponentA.jsx'
+import './index.css'
+import React from 'react';
 function App(){
 
      return (
-          <>
-          <MyComponent></MyComponent>
-          </>
-     )
+      <ComponentA/>
+     );
 }
 
 export default App
